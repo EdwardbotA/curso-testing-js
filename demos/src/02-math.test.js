a287b007b30d82104dc1354db92af1e86cc5c004
Edward B <ebotelloaroca00@gmail.com>
2024-06-27 19:06:29 -0500

    implementacion del setup de pruevas

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -1,41 +1,51 @@
 const { sum, multiply, divide, percentage } = require("./02-math.js");
 
-test('adds 1 + 3 should be 4', () => {
-  const rta = sum(1, 3);
-  expect(rta).toBe(4);
-});
+describe("Test for Math", () => {
+  describe("Test for sum", () => {
+    test("adds 1 + 3 should be 4", () => {
+      const rta = sum(1, 3);
+      expect(rta).toBe(4);
+    });
+  });
 
-test('should be 4', () => {
-  const rta = multiply(2, 2);
-  expect(rta).toBe(4);
-});
+  describe("Test for multiply", () => {
+    test("should be 4", () => {
+      const rta = multiply(2, 2);
+      expect(rta).toBe(4);
+    });
+  });
 
-test('should divide', () => {
-  const rta = divide(6, 3);
-  expect(rta).toBe(2);
+  describe("Test for divide", () => {
+    test("should divide", () => {
+      const rta = divide(6, 3);
+      expect(rta).toBe(2);
 
-  const rta2 = divide(5, 2);
-  expect(rta2).toBe(2.5);
-});
+      const rta2 = divide(5, 2);
+      expect(rta2).toBe(2.5);
+    });
 
-test('should divide for zero', () => {
-  const rta = divide(6, 0);
-  expect(rta).toBeNull();
+    test("should divide for zero", () => {
+      const rta = divide(6, 0);
+      expect(rta).toBeNull();
 
-  const rta2 = divide(5, 0);
-  expect(rta2).toBeNull();
-});
+      const rta2 = divide(5, 0);
+      expect(rta2).toBeNull();
+    });
+  });
 
-test('should return de percentage', () => {
-  const rta = percentage(1500, 50);
-  expect(rta).toBe(750);
+  describe("Test for percentage", () => {
+    test("should return de percentage", () => {
+      const rta = percentage(1500, 50);
+      expect(rta).toBe(750);
 
-  const rta2 = percentage(100, 25);
-  expect(rta2).toBe(25);
+      const rta2 = percentage(100, 25);
+      expect(rta2).toBe(25);
 
-  const rta3 = percentage(100, 10);
-  expect(rta3).toBe(10);
+      const rta3 = percentage(100, 10);
+      expect(rta3).toBe(10);
 
-  const rta4 = percentage(100, 90);
-  expect(rta4).toBe(90);
+      const rta4 = percentage(100, 90);
+      expect(rta4).toBe(90);
+    });
+  });
 });
