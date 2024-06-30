3a8e988927ce0805532abf1831cb24f2d4f50876
Edward B <ebotelloaroca00@gmail.com>
2024-06-29 19:41:36 -0500

    identificacion de la forma AAA de escribir pruebas

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -1,16 +1,24 @@
+// AAA = Arrange/alistar, Act/ejecutar, Assert/resolver.
+
 const Person = require("./06-person.js");
 
 describe("test for Person", () => {
   let person;
 
+  // Arrange
   beforeEach(() => {
     person = new Person("Edward", 45, 1.7);
   });
 
+
   test("Should return down", () => {
+    // Arrange
     person.weight = 45;
+
+    // Act
     const imc = person.calcIMC();
 
+    // Assert
     expect(imc).toBe("down");
   });
 
