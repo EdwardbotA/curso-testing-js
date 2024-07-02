99f124d587b2850af5a5a4c9f5f8862f9ca618d5
Edward B <ebotelloaroca00@gmail.com>
2024-07-01 21:01:00 -0500

    se agregan todos los archivos necesarion para poder hacer API test con unit test

---
+++ api/src/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+const { config } = require("./config");
+const createApp = require("./app");
+
+const app = createApp();
+app.listen(config.port, (err) => {
+  if (err) {
+    console.error("Error: ", err);
+  }
+});
