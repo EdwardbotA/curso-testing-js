f694bd214d5cd383acf43be178a77b67e8d80cd1
Edward B <ebotelloaroca00@gmail.com>
2024-07-02 18:34:01 -0500

    Crea un Mock para no tener alteraciones en el db

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -2,11 +2,26 @@
 /* eslint-disable quotes */
 const BooksService = require("./books.service");
 
+const fakeBooks = [
+  {
+    _id: 1,
+    name: "Harry Potter",
+  },
+];
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => {},
+};
+
+jest.mock("../lib/mongo.lib.js", () => jest.fn().mockImplementation(() => MongoLibStub));
+
 describe("Test for BooksService", () => {
   let service;
 
   beforeEach(() => {
     service = new BooksService();
+    jest.clearAllMocks();
   });
 
   describe("test for getBooks", () => {
@@ -16,7 +31,16 @@ describe("Test for BooksService", () => {
       const books = await service.getBooks();
       console.log(books);
       // Assert
-      expect(books.length).toEqual(2);
+      expect(books.length).toEqual(1);
+    });
+
+    test("should return a book list", async () => {
+      // Arrange
+      // Act
+      const books = await service.getBooks();
+      console.log(books[0].name);
+      // Assert
+      expect(books[0].name).toEqual("Harry Potter");
     });
   });
 });
